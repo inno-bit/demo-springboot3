0ae74045c7d95bf8b0b80d47cfced56cfbefb22b
inno <1597076708@qq.com>
2023-03-20 00:35:58 +0800

    init spring boot3 template

---
+++ src/main/java/com/example/demo/DemoSpringboot3demoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringboot3demoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringboot3demoApplication.class, args);
+    }
+
+}
